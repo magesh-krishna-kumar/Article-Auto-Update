957eb6291f5a1b45c6d39eb291cf1946048d84b2
magesh.kumar <magesh.kumar@travelport.com>
2022-04-05 19:02:33 +0530

    pom changes updated for selenium

--- src/test/java/com/travelport/articleupdate/impl/ArticleUpdate.java
+++ src/test/java/com/travelport/articleupdate/impl/ArticleUpdate.java
@@ -1,5 +1,10 @@
 package com.travelport.articleupdate.impl;
 
 public class ArticleUpdate {
+  
+  public static String updateArticle(String articleUri){
+    String status="";
+    return status;
+  }
 
 }
